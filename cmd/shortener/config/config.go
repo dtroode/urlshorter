eb1eba30ffe9e4e72b5607fa783750bee3740ce8
David Kistauri <davidkis113@gmail.com>
2025-03-03 17:57:42 +0300

    feat: iteration 5

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -1,11 +1,17 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/caarlos0/env/v6"
+)
 
 type Config struct {
-	RunAddr        string
-	BaseURL        string
-	ShortURLLength int
+	RunAddr        string `env:"SERVER_ADDRESS"`
+	BaseURL        string `env:"BASE_URL"`
+	ShortURLLength int    `env:"SHORT_URL_LENGTH"`
 }
 
 func ParseFlags() *Config {
@@ -15,6 +21,12 @@ func ParseFlags() *Config {
 	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "base url which goes before short url id")
 	flag.IntVar(&config.ShortURLLength, "l", 8, "short url id length")
 	flag.Parse()
+
+	err := env.Parse(config)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	return config
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require github.com/stretchr/testify v1.10.0
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/kr/text v0.2.0 // indirect
