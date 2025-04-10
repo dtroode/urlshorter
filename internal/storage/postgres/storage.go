5e4a4b9453735912b557508b9902abd66a66d4d7
David Kistauri <davidkis113@gmail.com>
2025-04-10 13:24:50 +0300

    chore: move back migrations

--- internal/storage/postgres/storage.go
+++ internal/storage/postgres/storage.go
@@ -8,7 +8,7 @@ import (
 	"github.com/dtroode/urlshorter/internal/model"
 	"github.com/dtroode/urlshorter/internal/storage"
 
-	// database "github.com/dtroode/urlshorter/postgres"
+	database "github.com/dtroode/urlshorter/postgres"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -20,9 +20,9 @@ type Storage struct {
 func NewStorage(dsn string) (*Storage, error) {
 	ctx := context.Background()
 
-	// if err := database.Migrate(ctx, dsn); err != nil {
-	// 	return nil, fmt.Errorf("failed to initialize database: %w", err)
-	// }
+	if err := database.Migrate(ctx, dsn); err != nil {
+		return nil, fmt.Errorf("failed to initialize database: %w", err)
+	}
 
 	pool, err := pgxpool.New(ctx, dsn)
 	if err != nil {
