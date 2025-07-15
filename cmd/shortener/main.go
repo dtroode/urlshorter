d3d0d87abb440005b5f59ade11c0a6e597b5192c
David Kistauri <davidkis113@gmail.com>
2025-07-15 13:20:56 +0300

    feat: iter 23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/dtroode/urlshorter/config"
 	"github.com/dtroode/urlshorter/internal/auth"
@@ -25,8 +27,8 @@ var (
 )
 
 func main() {
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, os.Interrupt)
+	defer stop()
 
 	config, err := config.Initialize()
 	if err != nil {
@@ -62,15 +64,23 @@ func main() {
 	r.RegisterAPIRoutes(urlService, jwt, logger)
 	r.RegisterHealthRoutes(healthService, logger)
 
+	server := &http.Server{
+		Addr:         config.RunAddr,
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
 	go func() {
 		var err error
 
 		if config.EnableHTTPS {
-			err = http.ListenAndServeTLS(config.RunAddr, config.CertFileName, config.PrivateKeyFileName, r)
+			err = server.ListenAndServeTLS(config.CertFileName, config.PrivateKeyFileName)
 		} else {
-			err = http.ListenAndServe(config.RunAddr, r)
+			err = server.ListenAndServe()
 		}
-		if err != nil {
+		if err != nil && err != http.ErrServerClosed {
 			logger.Fatal("error running server", "error", err)
 		}
 	}()
@@ -78,8 +88,15 @@ func main() {
 	logger.Info("server started", "address", config.RunAddr, "tls", config.EnableHTTPS)
 	logAppVersion()
 
-	<-sigChan
-	logger.Info("received interruption signal, exitting")
+	<-ctx.Done()
+	logger.Info("received interruption signal, shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Error("error during server shutdown", "error", err)
+	}
 }
 
 func logAppVersion() {
