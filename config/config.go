e1d78add5b743f5815ead9095545294581d1670d
David Kistauri <davidkis113@gmail.com>
2025-07-15 12:20:06 +0300

    feat: iter 22

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -10,43 +11,107 @@ import (
 
 // Config contains application configuration parameters.
 type Config struct {
-	RunAddr            string `env:"SERVER_ADDRESS"`
-	BaseURL            string `env:"BASE_URL"`
-	ShortKeyLength     int    `env:"SHORT_URL_LENGTH"`
-	LogLevel           string `env:"LOG_LEVEL"`
-	FileStoragePath    string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN        string `env:"DATABASE_DSN"`
-	JWTSecretKey       string `env:"JWT_SECRET_KEY"`
-	ConcurrencyLimit   int    `env:"CONCURRENCY_LIMIT"`
-	QueueSize          int    `env:"QUEUE_SIZE"`
-	EnableHTTPS        bool   `env:"ENABLE_HTTPS"`
-	CertFileName       string `env:"CERT_FILE_NAME"`
-	PrivateKeyFileName string `env:"PRIVATE_KEY_FILE_NAME"`
+	RunAddr            string `env:"SERVER_ADDRESS" json:"server_address"`
+	BaseURL            string `env:"BASE_URL" json:"base_url"`
+	ShortKeyLength     int    `env:"SHORT_URL_LENGTH" json:"short_url_length"`
+	LogLevel           string `env:"LOG_LEVEL" json:"log_level"`
+	FileStoragePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DatabaseDSN        string `env:"DATABASE_DSN" json:"database_dsn"`
+	JWTSecretKey       string `env:"JWT_SECRET_KEY" json:"jwt_secret_key"`
+	ConcurrencyLimit   int    `env:"CONCURRENCY_LIMIT" json:"concurrency_limit"`
+	QueueSize          int    `env:"QUEUE_SIZE" json:"queue_size"`
+	EnableHTTPS        bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+	CertFileName       string `env:"CERT_FILE_NAME" json:"cert_file_name"`
+	PrivateKeyFileName string `env:"PRIVATE_KEY_FILE_NAME" json:"private_key_file_name"`
+}
+
+func (c *Config) setDefaults() {
+	c.RunAddr = ":8080"
+	c.BaseURL = "http://localhost:8080"
+	c.ShortKeyLength = 8
+	c.LogLevel = "INFO"
+	c.FileStoragePath = fmt.Sprintf("%s/urls", os.TempDir())
+	c.DatabaseDSN = ""
+	c.JWTSecretKey = "a-string-secret-at-least-256-bits-long"
+	c.ConcurrencyLimit = 3
+	c.QueueSize = 0
+	c.EnableHTTPS = false
+	c.CertFileName = ""
+	c.PrivateKeyFileName = ""
 }
 
 // Initialize creates and initializes application configuration.
 func Initialize() (*Config, error) {
-	config := new(Config)
+	config := &Config{}
+	config.setDefaults()
 
-	flag.StringVar(&config.RunAddr, "a", ":8080", "address for app to run in format `host:port` or `:port`")
-	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "base url which goes before short url id")
-	flag.IntVar(&config.ShortKeyLength, "u", 8, "short key length")
-	flag.StringVar(&config.LogLevel, "l", "INFO", "log level")
-	flag.StringVar(&config.FileStoragePath, "f", fmt.Sprintf("%s/urls", os.TempDir()), "path to file where to store urls")
-	flag.StringVar(&config.DatabaseDSN, "d", "", "string for connecting to postgres")
-	flag.StringVar(&config.JWTSecretKey, "j", "a-string-secret-at-least-256-bits-long", "key to sign jwt")
-	flag.IntVar(&config.ConcurrencyLimit, "cl", 3, "number of workers in url service worker pool")
-	flag.IntVar(&config.QueueSize, "q", 0, "worker pool jobs queue size, if not passed, will be set based on concurrency limit")
-	flag.BoolVar(&config.EnableHTTPS, "s", false, "should server serve https")
-	flag.StringVar(&config.CertFileName, "sc", "", "cert file name")
-	flag.StringVar(&config.PrivateKeyFileName, "sp", "", "private key file name")
+	configFile := parseFlagsForConfigFile()
 
-	flag.Parse()
+	if configFile != "" {
+		if err := parseConfigFromFile(config, configFile); err != nil {
+			return nil, fmt.Errorf("failed to parse config from file: %w", err)
+		}
+	}
 
-	err := env.Parse(config)
-	if err != nil {
-		return nil, err
+	if err := parseCommandLineFlags(config); err != nil {
+		return nil, fmt.Errorf("failed to parse command line flags: %w", err)
+	}
+
+	if err := env.Parse(config); err != nil {
+		return nil, fmt.Errorf("failed to parse environment variables: %w", err)
 	}
 
 	return config, nil
+}
+
+// parseFlagsForConfigFile parses flags to get the config file path
+func parseFlagsForConfigFile() string {
+	var configFile string
+
+	tempFlagSet := flag.NewFlagSet("temp", flag.ContinueOnError)
+	tempFlagSet.StringVar(&configFile, "c", "", "config file name")
+
+	tempFlagSet.Parse(os.Args[1:])
+
+	if envConfigFile, set := os.LookupEnv("CONFIG"); set {
+		configFile = envConfigFile
+	}
+
+	return configFile
+}
+
+// parseCommandLineFlags parses all command line flags and applies them to config
+func parseCommandLineFlags(config *Config) error {
+	flagSet := flag.NewFlagSet("app", flag.ContinueOnError)
+
+	flagSet.String("c", "", "config file name")
+	flagSet.StringVar(&config.RunAddr, "a", config.RunAddr, "address for app to run in format `host:port` or `:port`")
+	flagSet.StringVar(&config.BaseURL, "b", config.BaseURL, "base url which goes before short url id")
+	flagSet.IntVar(&config.ShortKeyLength, "u", config.ShortKeyLength, "short key length")
+	flagSet.StringVar(&config.LogLevel, "l", config.LogLevel, "log level")
+	flagSet.StringVar(&config.FileStoragePath, "f", config.FileStoragePath, "path to file where to store urls")
+	flagSet.StringVar(&config.DatabaseDSN, "d", config.DatabaseDSN, "string for connecting to postgres")
+	flagSet.StringVar(&config.JWTSecretKey, "j", config.JWTSecretKey, "key to sign jwt")
+	flagSet.IntVar(&config.ConcurrencyLimit, "cl", config.ConcurrencyLimit, "number of workers in url service worker pool")
+	flagSet.IntVar(&config.QueueSize, "q", config.QueueSize, "worker pool jobs queue size, if not passed, will be set based on concurrency limit")
+	flagSet.BoolVar(&config.EnableHTTPS, "s", config.EnableHTTPS, "should server serve https")
+	flagSet.StringVar(&config.CertFileName, "sc", config.CertFileName, "cert file name")
+	flagSet.StringVar(&config.PrivateKeyFileName, "sp", config.PrivateKeyFileName, "private key file name")
+
+	return flagSet.Parse(os.Args[1:])
+}
+
+// parseConfigFromFile parses json file and applies values to config
+func parseConfigFromFile(config *Config, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s for reading: %w", filename, err)
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(config); err != nil {
+		return fmt.Errorf("failed to decode config file: %w", err)
+	}
+
+	return nil
 }
